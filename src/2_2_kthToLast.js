38c56b2bcbe5de7bed0a6eb602535a65cc670569
Adam Watt <awattny@me.com>
2017-05-31 15:03:28 -0700

    implement kthToLast

---
+++ src/2_2_kthToLast.js
@@ -0,0 +1,36 @@
+// General Methodology for Tech Interview Summary
+// STEP 1: Question
+// STEP 2: Test Case
+// STEP 3: Algo in English
+// STEP 4: Speed & Space
+// STEP 5: Code
+// STEP 6: Proof(OPT)
+// Return kth to Last: implement an algorithm to find the kth to last element of a singly linked list
+const kthToLast = ( head, k) => {
+  let first = head;
+  let second = head;
+  let currentPos = 1;
+  while ( second.next !== null ) {
+    if ( currentPos < k ) {
+      second = second.next;
+    } else {
+      first = first.next;
+      second = second.next;
+    }
+    currentPos++;
+  }
+  return first;
+};
+
+// l = current list size 
+
+// current kth to last =  l + k
+
+// look at iterative way and recursive way 
+
+// Iterative method expected O(n) Time O(1) space
+// Start at first node 
+// While node.next not null 
+//   
+
+module.exports = kthToLast;
