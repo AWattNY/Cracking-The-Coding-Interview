6191f28cb2a2a0bce53a17a3de49213a52e0ab14
Adam Watt <awattny@me.com>
2017-06-09 10:58:36 -0700

    re-implement Stack as a ES6 Classre

--- src/stackHelperFunctions.js
+++ src/stackHelperFunctions.js
@@ -1,15 +1,18 @@
-var Stack = function() {
+// export Stack1 for pseudo-Classical Instantiation 
+// export Stack for ES6 Classes
+
+var Stack1 = function() {
   this._storage = {};
   this._size = 0;
 };
 
 
-Stack.prototype.push = function(value) {
+Stack1.prototype.push = function(value) {
   this._storage[this._size] = value;
   this._size++;
 };
 
-Stack.prototype.pop = function() {
+Stack1.prototype.pop = function() {
   this._size && this._size--;
   var result = this._storage[this._size];
 
@@ -18,12 +21,43 @@ Stack.prototype.pop = function() {
   return result;
 };
 
-Stack.prototype.size = function() {
+Stack1.prototype.size = function() {
   return this._size;
 };
 
-Stack.prototype.peek = function() {
+Stack1.prototype.peek = function() {
   return this._storage[this._size - 1];
 };
+
+class Stack {
+  constructor() {
+    this._storage = {};
+    this._size = 0;
+  }  
+
+
+  push(value) {
+    this._storage[this._size] = value;
+    this._size++;
+  }
+
+  pop() {
+    this._size && this._size--;
+    var popped = this._storage[this._size];
+
+    delete this._storage[this._size];
+
+    return popped;
+  }
+
+  size() {
+    return this._size;
+  }
+
+  peek() {
+    return this._storage[this._size - 1];
+  }
+
+}
 
 module.exports = Stack;
