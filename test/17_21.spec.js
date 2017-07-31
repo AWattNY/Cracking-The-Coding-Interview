caa842cc72f4b067d03f154ccbc24b10c029ce5b
Adam Watt <awattny@me.com>
2017-07-30 18:45:18 -0700

    added tests for optimized algo

--- test/17_21.spec.js
+++ test/17_21.spec.js
@@ -1,4 +1,4 @@
-import getHistVolume from '../src/17_21_volumeOfHistogramme';
+import { getHistVolume, getHistVolumeOpt } from '../src/17_21_volumeOfHistogramme';
 import { expect } from 'chai';
 
 describe('Ctci - Hard Problems 17-21 volume Of Histogramme', function() {
@@ -16,6 +16,21 @@ describe('Ctci - Hard Problems 17-21 volume Of Histogramme', function() {
   it('should return 0 for histogramme with all decreasing values ', function() {
     var histogramme = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
     expect(getHistVolume(histogramme)).to.equal(0);
+  });
+
+  it('Optimized version should return correct water volume value for a given histogramme ', function() {
+    var histogramme = [0, 0, 4, 0, 0, 6, 0, 0, 3, 0, 5, 0, 1, 0, 0, 0];
+    expect(getHistVolumeOpt(histogramme)).to.equal(26);
+  });
+  
+  it('Optimized version should return correct water volume value for a given histogramme ', function() {
+    var histogramme = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+    expect(getHistVolumeOpt(histogramme)).to.equal(6);
+  });
+
+  it('Optimized version should return 0 for histogramme with all decreasing values ', function() {
+    var histogramme = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    expect(getHistVolumeOpt(histogramme)).to.equal(0);
   });
 
 });
