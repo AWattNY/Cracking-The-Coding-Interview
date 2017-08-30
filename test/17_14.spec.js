f1af4b71424403b4d7fdc94d50f76b3fc08c3e1e
Adam Watt <awattny@me.com>
2017-08-29 17:54:09 -0700

    implement tests for 17-14

---
+++ test/17_14.spec.js
@@ -0,0 +1,31 @@
+import kthSmallest from '../src/17_14_smallestK';
+import { expect } from 'chai';
+import _ from 'lodash';
+
+describe('Ctci - Hard Problems 17-14 Smallest K', function() {
+
+  it('should return 5 smallest elements of array ', function() {
+    var data = [55, -35, 26, 200, 0, 32, 18, 7, 23, 8, 2, 3];
+    var l = data.length - 1;
+    expect(_.isEqual(kthSmallest(data, 0, l, 5), [-35, 0, 2, 3, 7])).to.equal(true);
+  });
+  
+  it('should return 3 smallest elements of array ', function() {
+    var data = [55, -35, 26, 200, 0, 32, 18, 7, 23, 8, 2, 3];
+    var l = data.length;
+    expect(_.isEqual(kthSmallest(data, 0, l - 1, 3), [-35, 0, 2])).to.equal(true);
+  });
+
+  it('should return null for values of k smaller than 1 ', function() {
+    var data = [55, -35, 26, 200, 0, 32, 18, 7, 23, 8, 2, 3];
+    var l = data.length - 1;
+    expect(kthSmallest(data, 0, l, 0)).to.equal(null);
+  });
+
+  it('should return null for values of k larger than length of array ', function() {
+    var data = [55, -35, 26, 200, 0, 32, 18, 7, 23, 8, 2, 3];
+    var l = data.length - 1;
+    expect(kthSmallest(data, 0, l, 15)).to.equal(null);
+  });
+
+});
